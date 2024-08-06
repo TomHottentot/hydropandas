00098d66eafc870d8e426db45d430708cdd2466a
THottentot <tom.hottentot@rhdhv.com>
2024-08-06 12:51:19 +0200

    Test

--- tests/test_to_ipf.py
+++ tests/test_to_ipf.py
@@ -1,10 +1,11 @@
-import hydropandas as hdp
+#%%
+import hydropandas
 
 #%%
 my_extent = (167000, 172500, 472000, 476000) # Define a boundingbox (XMIN, XMAX, YMIN, YMAX)
 
 #%%
-BRO_Import = hdp.read_bro(extent=my_extent)
+BRO_Import = hydropandas.read_bro(extent=my_extent)
 
 #%%
 BRO_Import.to_IPF()
